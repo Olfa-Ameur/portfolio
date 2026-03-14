180d2dfc3536cf3a1fbd8b84f702c7b062974a6e
Olfa225 <olfaameur544@gmail.com>
2026-03-14 14:51:11 +0100

    Initial portfolio website

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("portfolio ready")
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  reveals.forEach((element) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = element.getBoundingClientRect().top;
+    const visiblePoint = 100;
+
+    if (elementTop < windowHeight - visiblePoint) {
+      element.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
